7db63527890d4ca34603dd9b6c9b51ddcf7fc4a6
Angel Nava <Angel Nava@DESKTOP-FN0S5C7>
2025-03-30 19:02:19 -0600

    Agregar archivos de proyecto.

---
+++ UserManagementApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; }
+    }
+}
